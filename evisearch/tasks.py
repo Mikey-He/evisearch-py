720cf6b5940440924b166fed36778b3f4dd3ee94
MkkyKK <hcfmike040210@gmail.com>
2025-11-17 18:09:08 -0800

    Update tasks.py

--- evisearch/tasks.py
+++ evisearch/tasks.py
@@ -68,7 +68,7 @@ def _rebuild_index_and_save() -> int:
                     ocrmypdf.ocr(
                         path,             # input_file
                         path,             # output_file (覆盖它自己)
-                        force_ocr=True,   # 强制 OCR，即使 PDF 已经有文本
+                        redo_ocr=True,   
                         language='eng',   # 假设是英语
                         jobs=1,           # 限制为1个核心，以免 Celery worker 过载
                         progress_bar=False,
